f5c38f05a51edc198803e6307717c76eb81b84bb
George Vilkitski <heorhi_vilkitski@epam.com>
2014-03-19 22:45:14 +0300

    change progress model

--- models/progress.js
+++ models/progress.js
@@ -1,25 +1,18 @@
-exports.progress = function(){
-    this.dates = [{
+exports.progress = function () {
+    this.dates = [
+        {
             date: Date.now(),
-            values: [{
-                teamRenaissancePages: [{
-                        key: "",
-                        progress: 0,
-                        person: "",
-                        uri: ""
-                }],
-                teamInspirationPages: [{
-                        key: "",
-                        progress: 0,
-                        person: "",
-                        uri: ""
-                }],
-                teamNovaPages: [{
-                        key: "",
-                        progress: 0,
-                        person: "",
-                        uri: ""
-                }]
-            }]
-        }];
+            teams: [
+                {
+                    name: "teamRenaissancePages",
+                    values: [
+                        {
+                            key: "",
+                            progress: 0,
+                            person: "",
+                            uri: ""
+                        }
+                    ]}
+            ]}
+    ];
 }
